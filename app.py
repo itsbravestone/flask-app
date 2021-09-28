01b4d48acaf2198a0388ecb3d783431e2fb6fde5
Steve <40924023+itsbravestone@users.noreply.github.com>
2021-09-28 20:45:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return "Hello there!"
+    return "Hello World!!!"
 
 @app.route('/dev')
 def dev():
     return make_response("Bravestone")
 
 @app.route('/hello')
-def dev():
+def hello():
     return make_response("Hello there")
 
 app.run(debug=True, port=8080)
