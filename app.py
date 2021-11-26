a8cd3ffb44ce68131e521e1da3ab1a85ecc743ee
Steve <40924023+itsbravestone@users.noreply.github.com>
2021-11-26 09:28:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,6 +30,38 @@ def test2():
     test = "test"
     return make_response("test", test)
 
+from flask import Flask, make_response
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return "Hello World!!!"
+
+@app.route('/dev')
+def dev():
+    return make_response("Bravestone")
+
+@app.route('/hello')
+def hello():
+    hello = "hey there"
+    return make_response("Hello there", hello)
+
+@app.route('/bye')
+def hello():
+    bye = "bye"
+    return make_response("bye", bye)
+
+@app.route('/test1')
+def test1():
+    test = "test"
+    return make_response("test", test)
+
+@app.route('/test3')
+def test3():
+    test = "test"
+    return make_response("test", test)
+
 @app.route('/test3')
 def test3():
     test = "test"
