7fc38a1ae3289e3b6ae71f373e0d8af0082eb2ed
Steve <40924023+itsbravestone@users.noreply.github.com>
2021-11-30 19:47:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def hello():
     return make_response("Hello there", hello)
 
 @app.route('/bye')
-def hello():
+def bye():
     bye = "bye"
     return make_response("bye", bye)
 
@@ -27,38 +27,6 @@ def test1():
 
 @app.route('/test2')
 def test2():
-    test = "test"
-    return make_response("test", test)
-
-from flask import Flask, make_response
-
-app = Flask(__name__)
-
-@app.route('/')
-def main():
-    return "Hello World!!!"
-
-@app.route('/dev')
-def dev():
-    return make_response("Bravestone")
-
-@app.route('/hello')
-def hello():
-    hello = "hey there"
-    return make_response("Hello there", hello)
-
-@app.route('/bye')
-def hello():
-    bye = "bye"
-    return make_response("bye", bye)
-
-@app.route('/test1')
-def test1():
-    test = "test"
-    return make_response("test", test)
-
-@app.route('/test3')
-def test3():
     test = "test"
     return make_response("test", test)
 
@@ -79,11 +47,6 @@ def test5():
 
 @app.route('/test6')
 def test6():
-    test = "test"
-    return make_response("test", test)
-
-@app.route('/test7')
-def test7():
     test = "test"
     return make_response("test", test)
 
