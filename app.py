2b0bc7f6b0e7ab9dbbea6b37576f7b088f55731a
Steve <40924023+itsbravestone@users.noreply.github.com>
2022-01-01 09:20:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,10 +15,10 @@ def hello():
     hello = "hey there"
     return make_response("Hello there", hello)
 
-@app.route('/bye')
-def bye():
-    bye = "bye"
-    return make_response("bye", bye)
+@app.route('/hey')
+def hey():
+    wish = "Happy New Year 2022"
+    return make_response("wish", wish)
 
 @app.route('/test1')
 def test1():
@@ -50,9 +50,5 @@ def test6():
     test = "test"
     return make_response("test", test)
 
-@app.route('/test7')
-def test7():
-    test = "test"
-    return make_response("test", test)
 
 app.run(debug=True, port=8080)
