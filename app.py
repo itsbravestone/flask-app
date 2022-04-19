32b5d3c19110900d963acfc6026974bca54e2ec4
Steve <40924023+itsbravestone@users.noreply.github.com>
2022-04-19 08:49:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -110,5 +110,10 @@ def test18():
     test = "test"
     return make_response("test", test)
 
+@app.route('/test19')
+def test19():
+    test = "test"
+    return make_response("test", test)
+
 
 app.run(debug=True, port=8080)
